9b8feed786f650a3507f1bdda2854616b8c12fa7
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-04-07 21:22:11 +0530

    Add hero component to about us page

--- src/pages/AboutUsPage/AboutUs.js
+++ src/pages/AboutUsPage/AboutUs.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import HeroSection from '../../modules/AboutUsModules/AboutUsHeroSection/HeroSection';
 import WhatWeDoSection from '../../modules/AboutUsModules/WhatWeDoSection/WhatWeDoSection';
 
 const AboutUs = () => {
   return (
     <div>
+      <HeroSection/>
       <WhatWeDoSection/>
     </div>
   )
