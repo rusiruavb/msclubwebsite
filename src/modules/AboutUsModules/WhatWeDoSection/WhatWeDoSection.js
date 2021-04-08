fa181da14b7905e4b3d04c314ad44a6b24bb2eff
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-04-08 17:49:29 +0530

    Create seperate component for display cards

--- src/modules/AboutUsModules/WhatWeDoSection/WhatWeDoSection.js
+++ src/modules/AboutUsModules/WhatWeDoSection/WhatWeDoSection.js
@@ -2,23 +2,23 @@ import React from 'react';
 import './WhatWeDoSection.css';
 import items from './WhatWeDoSection.json';
 
+const cards = items.data.map((item) => (
+  <div className="col-md-3 mb-3" key={item.id}>
+    <div className="card">
+      <div className="card-body">
+        <div className="d-flex justify-content-center">
+          <i className={item.icon} />
+        </div>
+        <p className="d-flex justify-content-center mt-3 content">{item.description}</p>
+      </div>
+    </div>
+  </div>
+));
+
 const WhatWeDoSection = () => (
   <div className="container">
     <h1 className="text-center mt-4 title">What We Do?</h1>
-    <div className="row mt-4">
-      {items.data.map((item) => (
-        <div className="col-md-3 mb-3" key={item.id}>
-          <div className="card">
-            <div className="card-body">
-              <div className="d-flex justify-content-center">
-                <i className={item.icon} />
-              </div>
-              <p className="d-flex justify-content-center mt-3 content">{item.description}</p>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
+    <div className="row mt-4">{cards}</div>
   </div>
 );
 
