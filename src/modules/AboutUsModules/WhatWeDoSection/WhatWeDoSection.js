33c23286bb582eb334b3d8a2b72ad1f4a60c04d2
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-04-09 21:21:03 +0530

    Convert file extention to .jsx

--- src/modules/AboutUsModules/WhatWeDoSection/WhatWeDoSection.js
+++ src/modules/AboutUsModules/WhatWeDoSection/WhatWeDoSection.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import './WhatWeDoSection.css';
+import PropTypes from 'prop-types';
 import items from './WhatWeDoSection.json';
 
-const cards = items.data.map((item) => (
-  <div className="col-md-3 mb-3" key={item.id}>
-    <div className="card">
-      <div className="card-body">
-        <div className="d-flex justify-content-center">
-          <i className={item.icon} />
-        </div>
-        <p className="d-flex justify-content-center mt-3 content">{item.description}</p>
+const Card = ({ icon, description }) => (
+  <div className="card">
+    <div className="card-body">
+      <div className="d-flex justify-content-center">
+        <i className={icon} />
       </div>
+      <p className="d-flex justify-content-center mt-3 content">{description}</p>
     </div>
   </div>
-));
+);
+
+Card.propTypes = {
+  icon: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
 const WhatWeDoSection = () => (
   <div className="container">
     <h1 className="text-center mt-4 title">What We Do?</h1>
-    <div className="row mt-4">{cards}</div>
+    <div className="row mt-4">
+      {items.data.map((item) => (
+        <div className="col-md-3 mb-3" key={item.id}>
+          <Card icon={item.icon} description={item.description} />
+        </div>
+      ))}
+    </div>
   </div>
 );
 
