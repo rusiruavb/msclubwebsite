a223084d333b7d2f9fcd9fbff19c824da61bae24
Senura Vihan Jayadeva <53615169+senuravihanjayadeva@users.noreply.github.com>
2021-04-12 10:54:30 +0530

    Add what we do section - Home Page (#35)
    * Completed What We Do Section -Home Page
    
    * Changed the route order
    
    * Add What We Do Section - Home Page

---
+++ src/pages/HomePage/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import WhatWeDoSection from '../../modules/HomeModules/WhatWeDoSection/WhatWeDoSection';
+
+const Home = () => (
+  <div>
+    <WhatWeDoSection />
+  </div>
+);
+
+export default Home;
