ce2c1badd29001814f749325bcd43559a3767bc8
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2021-04-12 11:24:39 +0530

    Footer #9 finished (#26)
    * add bootstrap
    
    * add npm @fortawesome/fontawesome-svg-core,@fortawesome/free-solid-svg-icons,@fortawesome/react-fontawesome
    
    * Footer finished
    
    * Footer mobile responsive also finished
    
    * footer logo added
    
    * make as functional component
    
    * create css variables
    
    * resolving all conversation
    
    * logo added
    
    * css variables created
    
    Co-authored-by: gsir2992 <gihan.siriwardhana@syscolabs.com>

---
+++ src/component/common/footer/Footer.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import logo from '../../../assets/images/logo/Ms-Club.jpg';
+
+const footer = () => (
+  <div className="container-fluid">
+    <div className="row footerDiv justify-content-lg-around text-light ">
+      <div className="ml-md-5 ml-lg-2 col-sm-3 col-md-3 col-lg-3 d-flex flex-column justify-content-between">
+        <div className="mb-sm-4 mt-sm-4 mb-md-4 mt-md-4 mt-lg-4 logoDiv">
+          <img src={logo} alt="logo" width="142px" height="142px" />
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti magni maiores molestias suscipit tempore?
+            Asperiores aut beatae doloremque, enim fugit, illum itaque mollitia nobis optio quo ratione, repudiandae
+            voluptas voluptates.
+          </p>
+        </div>
+      </div>
+      <div className="col-sm-3 col-md-3 col-lg-3">
+        <div className="mb-sm-4 mt-sm-4 mb-md-4 mt-md-4 mt-lg-4 useFullLinkDiv">
+          <h5>Usefull Links</h5>
+          <span />
+          <div>
+            <ul className="navbar-nav">
+              <li className="nav-link">Home</li>
+              <li className="nav-link">Contact Us</li>
+              <li className="nav-link">Blog</li>
+              <li className="nav-link">Events</li>
+              <li className="nav-link">Code of Contact</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-4 col-md-4 col-lg-4">
+        <div className="mb-sm-4 mt-sm-4 mb-md-4 mt-md-4 mt-lg-4 subscribeDetailsDiv">
+          <h5>Subscribe</h5>
+          <span id="subscribeUnderLineSpan" />
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid asperiores dolores harum laboriosam,
+              maiores nobis quae repudiandae tempora voluptatem? Autem esse facilis neque nihil nobis perspiciatis
+              soluta sunt vero!
+            </p>
+          </div>
+          <div className="mb-md-4 mt-md-4 mt-lg-4">
+            <div className="input-group textFieldDiv">
+              <input type="text" className="form-control emailAddress" placeholder="Email Address" />
+              <span className="input-group-text icon">
+                <FontAwesomeIcon className="emailIcon" icon={faEnvelope} transform={{ rotate: -10 }} />
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="row copyRightText">
+      <div
+        className="col-sm-12 col-md-12 col-lg-12 text-light d-flex justify-content-lg-start
+                        justify-content-sm-center align-items-center"
+      >
+        <div className="pt-3 pb-3 copyRightTextDiv">
+          <p className="mb-0">Copyright © 2021 | All Right Reserved MS Club of SLIIT</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default footer;
---
+++ src/pages/AboutUsPage/AboutUs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Footer from '../../component/common/footer/Footer';
+
+const aboutUs = () => (
+  <div>
+    <Footer />
+  </div>
+);
+
+export default aboutUs;
