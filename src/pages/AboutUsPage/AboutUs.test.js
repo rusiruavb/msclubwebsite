8a529638660a2ef140c21ec70935f008eda8f1ee
Amith Mihiranga <39789194+lwpamihiranga@users.noreply.github.com>
2021-04-19 10:20:52 +0530

    Introducing Unit Tests (#36)
    * update test for App.js
    
    * init tests for HeroSection.jsx
    
    * init tests for MissionVission.jsx
    
    * init tests for WhatMsClub.jsx
    
    * init tests for WhatWeDoSection.jsx
    
    * test suites naming correction
    
    * init tests for HomeHeroSection.jsx
    
    * init tests for HomeModule - WhatWeDoSection.test.jsx
    
    * init tests for Footer component
    
    * correctiong spelling mistake
    
    * description text change in AboutUsModule tests
    
    * init tests for HomePage
    
    * init tests for AboutUs page
    
    * fixing jQuery error in testing
    
    Co-authored-by: Chamod Shehanka <hcsperera@outlook.com>

--- src/pages/AboutUsPage/AboutUs.test.js
+++ src/pages/AboutUsPage/AboutUs.test.js
@@ -1,11 +1,11 @@
 /* global describe, test, document */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AboutUs from './AboutUs';
 
-describe('App.js', () => {
+describe('AboutUs Page', () => {
   test('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<AboutUs />, div);
   });
 });
--- src/pages/HomePage/Home.test.js
+++ src/pages/HomePage/Home.test.js
@@ -1,11 +1,11 @@
 /* global describe, test, document */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Home from './Home';
 
-describe('App.js', () => {
+describe('Home Page', () => {
   test('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<Home />, div);
   });
 });
