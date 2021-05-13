47a96c511af653b5588231479346b067957def1b
Senura Vihan Jayadeva <53615169+senuravihanjayadeva@users.noreply.github.com>
2021-05-13 14:39:10 +0530

    Completed Upcoming Event Component (#42)

--- src/pages/EventsPage/Events.jsx
+++ src/pages/EventsPage/Events.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PastEvents from '../../modules/EventsModule/pastEvents/PastEvents';
+import UpcomingEventComponent from '../../modules/EventsModule/UpcomingEventComponent/UpcomingEventComponent';
 
 const Events = () => (
   <div>
+    <UpcomingEventComponent />
     <PastEvents />
   </div>
 );
