dbaca07c170eb2a58e30ed717d507b972e0cccd6
Dinuwan Kalubowila <kalubowila.dc25132@live.com>
2021-06-02 07:42:13 +0530

    footer added to the Home,Events

--- src/pages/EventsPage/Events.jsx
+++ src/pages/EventsPage/Events.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PastEvents from '../../modules/EventsModule/pastEvents/PastEvents';
 import UpcomingEventComponent from '../../modules/EventsModule/UpcomingEventComponent/UpcomingEventComponent';
+import Footer from '../../component/common/footer/Footer';
 
 const Events = () => (
   <div>
     <UpcomingEventComponent />
     <PastEvents />
+    <Footer />
   </div>
 );
 
