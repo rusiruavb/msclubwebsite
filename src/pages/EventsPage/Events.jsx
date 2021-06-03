13489e44fee0afb3be1f3113706cad53240bbdb4
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2021-06-03 19:55:09 +0530

    Done - Events Page Hero Section #10 (#48)

--- src/pages/EventsPage/Events.jsx
+++ src/pages/EventsPage/Events.jsx
@@ -3,9 +3,11 @@ import PastEvents from '../../modules/EventsModule/pastEvents/PastEvents';
 import UpcomingEventComponent from '../../modules/EventsModule/UpcomingEventComponent/UpcomingEventComponent';
 import Footer from '../../component/common/footer/Footer';
 import FutureEvents from '../../modules/EventsModule/futureEvents/FutureEvents';
+import EventHeroSection from '../../modules/EventsModule/heroSection/EventHeroSection';
 
 const Events = () => (
   <div>
+    <EventHeroSection />
     <UpcomingEventComponent />
     <PastEvents />
     <FutureEvents />
