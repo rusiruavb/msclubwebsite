7f874635af13e59dc9c1e64b8f6a029cc245da42
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-06-20 13:36:54 +0530

    Modify responsive break points in about us hero section

--- src/modules/AboutUsModules/AboutUsHeroSection/HeroSection.jsx
+++ src/modules/AboutUsModules/AboutUsHeroSection/HeroSection.jsx
@@ -1,26 +1,23 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
 import './HeroSection.css';
 import headerImage from '../../../assets/about-us/header-img.svg';
 import Navbar from '../../../component/common/navbar/Navbar';
 
 const HeroSection = () => (
-  <div className="overlay">
+  <div className="container-fluid overlay">
     <div className="hero-section-bg" />
     <Navbar />
     <div className="row">
       <div className="col-md-6">
-        <p className="header-title">About US</p>
-        <div>
-          <p className="header-content">
-            A student-driven community based on Sri Lanka Institute of Information Technology aiming to bridge the skill
-            gap between an Undergraduate and an Industry Professional.
-          </p>
-        </div>
+        <p className="header-title">About Us</p>
+        <p className="header-content">
+          A student-driven community based on Sri Lanka Institute of Information Technology aiming to bridge the skill
+          gap between an Undergraduate and an Industry Professional.
+        </p>
       </div>
       <div className="col-md-6 header-img">
         <div className="d-flex justify-content-right">
-          <img src={headerImage} alt="About Us header image" className="d-none d-sm-block header-img" />
+          <img src={headerImage} alt="About Us header" className="d-none d-sm-block header-img" />
         </div>
       </div>
     </div>
