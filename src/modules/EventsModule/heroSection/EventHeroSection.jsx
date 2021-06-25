f096be9ab0fe3019fdf75ecd4dc99eb54ecd4f91
rusiruavb <rusiruavb98@gmail.com>
2021-06-25 13:34:04 +0530

    Add responsive alignments to event hero section

--- src/modules/EventsModule/heroSection/EventHeroSection.jsx
+++ src/modules/EventsModule/heroSection/EventHeroSection.jsx
@@ -8,10 +8,9 @@ const EventHeroSection = () => (
     <div className="row">
       <div className="col-sm-12 col-md-6 col-lg-6 text-div">
         <div className="col-12">
-          <p className="exploreText">Explore </p>
-          <p className="exploreText">events</p>
+          <p className="exploreText">Explore events</p>
         </div>
-        <div className="col-12">
+        <div className="col-12 event-search">
           <input type="text" className="form-control searchBox" />
           <button type="button" className="btn searchButton">
             <i className="fa fa-search" />
