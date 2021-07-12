3eaac5555896a518644b15c5ab29729b9b51bd0c
Himasha Weerasinghe <68691192+himashatw@users.noreply.github.com>
2021-07-12 18:07:16 +0530

    Speaker UI enhancement (#65)
    * redesigned speaker UI
    
    * speaker json added
    
    * merged changes from master

--- src/component/TopSpeakerCard/TopSpeakerCard.jsx
+++ src/component/TopSpeakerCard/TopSpeakerCard.jsx
@@ -2,11 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './SpeakerCardStyle.css';
 
-const TopSpeakerCard = ({ image, title }) => (
-  <div className="speaker-card card mb-2 mt-2">
-    <img className="past-speaker-img" src={image} alt="Card cap" />
-    <div className="card-body">
-      <h5 className="card-title">{title}</h5>
+const TopSpeakerCard = ({ image, title, description, urls }) => (
+  <div className="Card">
+    <div className="upper-container">
+      <div className="image-container">
+        <img src={image} alt="speakerIMG" />
+      </div>
+    </div>
+    <div className="lower-container">
+      <h3>{title}</h3>
+      <h4>{description}</h4>
+      {urls.fb && (
+        <a href={urls.fb} target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-facebook fa-2x" />
+        </a>
+      )}
+      {urls.instagram && (
+        <a href={urls.instagram} target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-instagram fa-2x" />
+        </a>
+      )}
+      {urls.twitter && (
+        <a href={urls.twitter} target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-twitter fa-2x" />
+        </a>
+      )}
+      {urls.linkedIn && (
+        <a href={urls.linkedIn} target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin fa-2x" />
+        </a>
+      )}
+      {urls.web && (
+        <a href={urls.web} target="_blank" rel="noopener noreferrer">
+          <i className="fas fa-globe fa-2x" style={{ color: 'black' }} />
+        </a>
+      )}
     </div>
   </div>
 );
@@ -14,6 +44,8 @@ const TopSpeakerCard = ({ image, title }) => (
 TopSpeakerCard.propTypes = {
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  urls: PropTypes.object.isRequired,
 };
 
 export default TopSpeakerCard;
