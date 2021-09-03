c83c10ba1d11a8ac1e2a7b804fd910f0c3af4643
CandyPanda-LS <it19132310@my.sliit.lk>
2021-09-03 09:58:13 +0530

    blogs/blog api

--- src/component/BlogCard/BlogCard.jsx
+++ src/component/BlogCard/BlogCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import './BlogCardStyle.css';
 import moment from 'moment';
 
-const BlogCard = ({ image, title, tags, datetime, description, link, mediumProfileImg }) => (
+const BlogCard = ({ image, title, tags, datetime, link }) => (
   <div className="blog-card card mb-3 mt-3">
     <div>
       <img className="img-fluid blog-card-img" src={image} alt="Card cap" />
@@ -18,9 +18,6 @@ const BlogCard = ({ image, title, tags, datetime, description, link, mediumProfi
         ))}
       <div>
         <div className="row blog-card-header-row">
-          <div className="col-md-1 col-sm-2 col-2 mt-2 mb-2 blog-card-header-col1">
-            <img className="img-fluid rounded-circle mediumProfileImg" src={mediumProfileImg} alt="mediumImg" />
-          </div>
           <div className="col-md-10 col-sm-10 col-10 mt-1 blog-card-header-col2">
             <h5 className="card-title text-left">{title}</h5>
           </div>
@@ -29,7 +26,6 @@ const BlogCard = ({ image, title, tags, datetime, description, link, mediumProfi
           <i className="fas fa-calendar-alt fa-lg mt-1" />
           <span className="badge rounded-pill event-date text-muted">{moment(datetime).format('lll')}</span>
         </div>
-        <p className="description">{description}</p>
       </div>
 
       <div className="d-flex justify-content-center">
@@ -48,9 +44,7 @@ BlogCard.propTypes = {
   title: PropTypes.string.isRequired,
   tags: PropTypes.array.isRequired,
   datetime: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
-  mediumProfileImg: PropTypes.string.isRequired,
 };
 
 export default BlogCard;
